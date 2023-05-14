b4661c856f894f116f1c1ada96e9af65236b05c2
Andrew Kushyk <61907955+git-user-cpp@users.noreply.github.com>
2023-05-14 11:46:58 +0300

    Update main.cpp

--- Database/main.cpp
+++ Database/main.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2022 m!haly4
+Copyright (c) 2022 Andrew Kushyk
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
